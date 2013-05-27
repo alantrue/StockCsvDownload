0a36ac12ec9ee8cb10ae1567be54355fcd8282e2
alan chen <alantruekimo@yahoo.com.tw>
2013-05-27 16:15:28 +0800

    fit to python 2.7

--- download.py
+++ download.py
@@ -1,13 +1,16 @@
 import string
 import codecs
-import urllib.request
-import configparser
+import urllib
+import ConfigParser
 import os
 import datetime
 
 from datetime import date, timedelta
 
-for i in range(4530):
+end = datetime.date(2004, 2, 10)
+delta = date.today() - end
+
+for i in range(delta.days):
     mon = (date.today() + timedelta(-i)).strftime('%Y%m')
     day = (date.today() + timedelta(-i)).strftime('%Y%m%d')
     #print(mon)
@@ -16,6 +19,6 @@ for i in range(4530):
     url = "http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX3_print.php?genpage=genpage/Report{0}/A112{1}ALLBUT0999_1.php&type=csv".format(mon, day)
     file = "E:\\stockData\\{0}.csv".format(day)
 
-    urllib.request.urlretrieve(url, file)
+    urllib.urlretrieve(url, file)
 
 print("done")
