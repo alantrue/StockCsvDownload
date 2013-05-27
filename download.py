33b574af1dcb15ec791c0d48e6344279a2eaa7b0
alan chen <alantruekimo@yahoo.com.tw>
2013-05-27 13:54:21 +0800

    change download path

--- download.py
+++ download.py
@@ -7,14 +7,14 @@ import datetime
 
 from datetime import date, timedelta
 
-for i in range(144):
+for i in range(4530):
     mon = (date.today() + timedelta(-i)).strftime('%Y%m')
     day = (date.today() + timedelta(-i)).strftime('%Y%m%d')
     #print(mon)
     #print(day)
 
     url = "http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX3_print.php?genpage=genpage/Report{0}/A112{1}ALLBUT0999_1.php&type=csv".format(mon, day)
-    file = "E:\\Python\\download\\data\\{0}.csv".format(day)
+    file = "E:\\stockData\\{0}.csv".format(day)
 
     urllib.request.urlretrieve(url, file)
 
